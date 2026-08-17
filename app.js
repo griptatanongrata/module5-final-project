9cf678daebe28f9ee6da9d389e949fe61279801a
griptatanongrata <j.ttipton@yahoo.com>
2026-08-17 16:34:51 -0500

    Module 5 Final Project

---
+++ app.js
@@ -0,0 +1,23 @@
+//API 1: http://www.omdbapi.com/?apikey=37adbdf5&
+
+async function main() {
+  const movies = await fetch(`http://www.omdbapi.com/?apikey=37adbdf5&s=`);
+  const movieData = await movies.json();
+  const movieListEl = document.querySelector(`.movie-list`);
+  console.log(movieData);
+  movieListEl.innerHTML = movieData
+    .map(
+      (movie) =>
+        `<div class="movie-card">
+      <figure class="movie-card__container">
+      <img class="movie-poster" src="${movie.Poster} alt=""></figure>
+      <div class="movie-title">${movie.Title}</div>
+      <div class="movie-year">${movie.Year}</div>
+      <div class="movie-id">${movie.imdbID}</div>
+      </div>
+      `,
+    )
+    .join("");
+}
+
+main();
